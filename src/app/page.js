9cd5f73e2cb97503d7e76c9cb2da60c36b1f14e3
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-02-19 16:01:14 +0530

    Add home header component

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
+import HomeHeader from "@/components/HomeHeader";
 
 export default function Home() {
   return (
-    <div className="text-red-500">Home</div>
+    <>
+      <HomeHeader />
+    </>
   )
 }
