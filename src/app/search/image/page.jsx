6f76d96a74df0eef7f3a103d3998c6bb0667368d
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-02-24 18:16:36 +0530

    Add loading effects for web and image pages

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -5,6 +5,7 @@ import React from 'react'
 
 export default async function ImageSearchPage({ searchParams }) {
     const startIndex = searchParams.start || "1";
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     const response = await fetch(
         `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${startIndex}`
     );
--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -4,6 +4,7 @@ import WebSearchResults from '../../../components/WebSearchResults';
 
 export default async function WebSearchPage({ searchParams }) {
     const startIndex = searchParams.start || "1";
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     const response = await fetch(
         `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${startIndex}`
     );
