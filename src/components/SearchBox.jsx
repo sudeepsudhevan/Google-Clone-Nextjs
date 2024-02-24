6eeda064b33284b658573e78dde115640c75a5e1
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2024-02-24 19:13:05 +0530

    added suspense in searcbox

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -2,7 +2,7 @@
 import { RxCross2 } from "react-icons/rx";
 import { BsFillMicFill } from "react-icons/bs";
 import { AiOutlineSearch } from "react-icons/ai";
-import { useState } from "react";
+import { Suspense, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 
 export default function SearchBox() {
@@ -17,16 +17,18 @@ export default function SearchBox() {
         router.push(`/search/web?searchTerm=${term}`)
     }
     return (
-        <form onSubmit={handleSubmit} className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow 
+        <Suspense>
+            <form onSubmit={handleSubmit} className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow 
         max-w-3xl items-center">
-            <input type="text" className="w-full focus:outline-none" value={term}
-                onChange={(e) => setTerm(e.target.value)} />
-            <RxCross2 className="text-2xl text-gray-500 cursor-pointer sm:mr-2"
-                onClick={() => setTerm("")} />
-            <BsFillMicFill className="hidden sm:inline-flex text-4xl text-blue-500 border-l-2 
+                <input type="text" className="w-full focus:outline-none" value={term}
+                    onChange={(e) => setTerm(e.target.value)} />
+                <RxCross2 className="text-2xl text-gray-500 cursor-pointer sm:mr-2"
+                    onClick={() => setTerm("")} />
+                <BsFillMicFill className="hidden sm:inline-flex text-4xl text-blue-500 border-l-2 
             border-gray-300 mr-3 pl-4" />
-            <AiOutlineSearch className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer"
-                onClick={handleSubmit} />
-        </form>
+                <AiOutlineSearch className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer"
+                    onClick={handleSubmit} />
+            </form>
+        </Suspense>
     )
 }
